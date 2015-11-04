74321ca9baaa402d3a2d4c7287cb0b5c303738c7
Rosalyn Koscica <rkoscica@rkoscica-2016.local>
2015-11-04 09:05:02 -0500

    Finished 3.2.5 - now stores user preferences from last run

--- RGB Sliders/ViewController.swift
+++ RGB Sliders/ViewController.swift
@@ -17,12 +17,14 @@ class ViewController: UIViewController {
      @IBOutlet weak var colorSquare: UIView!
     
      override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
-        colorSquare.layer.borderWidth = 1
-        updateSquare()
+          super.viewDidLoad()
+          colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+          colorSquare.layer.borderWidth = 1
+          let defaults = NSUserDefaults.standardUserDefaults()
+          redSlider.value = defaults.floatForKey("red")
+          greenSlider.value = defaults.floatForKey("green")
+          blueSlider.value = defaults.floatForKey("blue")
+          updateSquare()
      }
 
      override func didReceiveMemoryWarning() {
@@ -30,9 +32,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
         print("Memory Warning")
      }
+     
+     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+          if (segue.identifier == "openColor") {
+               let newViewController = segue.destinationViewController
+               newViewController.view.backgroundColor = colorSquare.backgroundColor
+          }
+     }
 
      @IBAction func updateSquare() {
         colorSquare.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+          let defaults = NSUserDefaults.standardUserDefaults()
+          defaults.setFloat(redSlider.value, forKey: "red")
+          defaults.setFloat(greenSlider.value, forKey: "green")
+          defaults.setFloat(blueSlider.value, forKey: "blue")
+          defaults.synchronize()
     }
     
 
